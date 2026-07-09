d41355ff220b5c6bb79609f681e73fc6c1b8efc3
Abhay Singh Tomar <86891124+abhaysinghtomar97@users.noreply.github.com>
2026-07-09 11:02:14 +0530

    Update AktuResult.jsx

--- src/components/AktuResult.jsx
+++ src/components/AktuResult.jsx
@@ -38,7 +38,7 @@ export default function AktuResult() {
 
   return (
     <div className="max-w-5xl mx-auto p-6 space-y-6">
-      <div className="bg-white rounded-xl shadow border p-5">
+      <div className=" rounded-xl shadow border p-5">
         <h2 className="text-2xl font-bold mb-4">
           AKTU Result Checker
         </h2>
@@ -70,7 +70,7 @@ export default function AktuResult() {
       {result && (
         <>
           {/* Student Info */}
-          <div className="bg-white rounded-xl border shadow p-6">
+          <div className=" rounded-xl border shadow p-6">
             <h3 className="text-xl font-semibold mb-4">
               Student Details
             </h3>
@@ -105,7 +105,7 @@ export default function AktuResult() {
           {result.semesters.map((sem) => (
             <div
               key={sem.semester}
-              className="bg-white rounded-xl border shadow p-6"
+              className=" rounded-xl border shadow p-6"
             >
               <div className="flex justify-between flex-wrap gap-3 mb-5">
                 <div>
